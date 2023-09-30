0123b95391ab38b25c7f7ccb5e4b71c048df46af
Bartłomiej Kowalczuk <92793918+bartek623@users.noreply.github.com>
2023-09-30 19:30:29 +0200

    map (#6)
    * clear app
    
    * client app structure setup
    
    * leaflet map
    
    * package lock
    
    * page layout
    
    ---------
    
    Co-authored-by: bartek623 <bartlomiej.kowalczuk12@gmail.com>

---
+++ apps/client/src/app/components/Map/Map.tsx
@@ -0,0 +1,16 @@
+import { MapContainer, TileLayer } from 'react-leaflet';
+
+export function Map() {
+    return (
+        <MapContainer
+            style={{ width: '100%', height: '100%' }}
+            center={[51.505, -0.09]}
+            zoom={13}
+        >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            />
+        </MapContainer>
+    );
+}
