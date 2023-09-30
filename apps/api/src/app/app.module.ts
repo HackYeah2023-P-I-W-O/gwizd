0a94e2da1fa9519f3ba312bb4298b880ac65f4fb
Maciej Opaliński <poprostumieciek@gmail.com>
2023-09-30 18:57:40 +0200

    feat(api): Import to `AppModule`

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '../config';
 import { AppController, AppService } from '.';
 import { DatabaseModule } from '../database';
+import { AuthModule } from '../auth';
+import { UserModule } from '../user';
 
 @Module({
-    imports: [ConfigModule, DatabaseModule],
+    imports: [ConfigModule, DatabaseModule, AuthModule, UserModule],
     controllers: [AppController],
     providers: [AppService],
 })
