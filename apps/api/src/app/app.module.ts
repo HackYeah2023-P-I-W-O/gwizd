75b5ebba1549721e050e4ca4c02ef6badfb00548
Jakub Żojdzik <69974199+JakubZojdzik@users.noreply.github.com>
2023-09-30 19:06:10 +0200

    Kuba (#4)
    * Add Point controller
    
    * Add point.entity
    
    * Add points getAll endpoint
    
    * Add point delete

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,9 +4,16 @@ import { AppController, AppService } from '.';
 import { DatabaseModule } from '../database';
 import { AuthModule } from '../auth';
 import { UserModule } from '../user';
+import { PointModule } from '../point/point.module';
 
 @Module({
-    imports: [ConfigModule, DatabaseModule, AuthModule, UserModule],
+    imports: [
+        ConfigModule,
+        DatabaseModule,
+        AuthModule,
+        UserModule,
+        PointModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
