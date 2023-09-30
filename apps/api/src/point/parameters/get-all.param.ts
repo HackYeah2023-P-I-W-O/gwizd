900dbf4d5aeabe15ea7a5fa12a3d420a24183420
Jakub Żojdzik <69974199+JakubZojdzik@users.noreply.github.com>
2023-09-30 20:21:55 +0200

    Kuba (#9)
    * Add Point controller
    
    * Add point.entity
    
    * Add points getAll endpoint
    
    * Add point delete
    
    * Add point create endpoint
    
    * Fix point endpoints
    
    * Add edit point endpoint

--- apps/api/src/point/parameters/get-all.param.ts
+++ apps/api/src/point/parameters/get-all.param.ts
@@ -1,6 +1,22 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { PointCategory } from '../point.entity';
 
 export class GetAllPointParam {
+    @ApiPropertyOptional({ enum: PointCategory })
     category?: PointCategory;
+    @ApiPropertyOptional()
     danger?: boolean;
+}
+
+export class CreatePointParam {
+    @ApiProperty({ enum: PointCategory })
+    category: PointCategory;
+    @ApiProperty()
+    location: string;
+    @ApiPropertyOptional()
+    description?: string;
+    @ApiPropertyOptional()
+    photo?: string;
+    @ApiProperty()
+    danger: boolean;
 }
